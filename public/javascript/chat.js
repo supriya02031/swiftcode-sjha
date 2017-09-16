359a84413602120715386227e3c400ae4cb8bcbf
Live session user <supriya.sxs@gmail.com>
2017-09-16 11:49:33 +0000

    commit

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -1,7 +1,7 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function($scope, $sce){
 	$scope.messages =[];
-var  exampleSocket =  new  WebSocket("wss://swiftcode-sjha/chatSocket");
+var  exampleSocket =  new  WebSocket("wss://swiftcode-sjha.herokuapp.com/chatSocket");
 exampleSocket.onmessage  =   function  (event) {
          var jsonData = JSON.parse(event.data);
         jsonData.time = new Date().toLocaleTimeString();
